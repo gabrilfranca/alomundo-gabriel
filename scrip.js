24dcf8461678252b568ee9f43a669f714c832f26
gabrilfranca <164918506+gabrilfranca@users.noreply.github.com>
2025-08-01 15:00:57 -0300

    ghffghdd

--- scrip.js
+++ scrip.js
@@ -4,5 +4,9 @@ function inserirNome(){
     console.log(elemento);
     elemento.textContent = nomeUsuario;
 }
+const lista = document.querySelector('#lista');
+lista.textContent = lingugens[0];
+lista.textContent = lingugens[1];
+lista.textContent = lingugens[2];
 
 
